1360fa87e0b8833ddcbf5c1b39815cbe98ba3fd5
zestra <zestra@zestra-VirtualBox>
2020-11-11 16:55:13 -0500

    fin

--- FileIO.cpp
+++ FileIO.cpp
@@ -13,12 +13,43 @@ using namespace std;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+
+	ifstream myInputfile;
+	myInputfile.open(filename.c_str());
+
+	if (!myInputfile.is_open()){
+		contents = "";
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	std::string line;
+	while (!myInputfile.eof()) {
+			getline(myInputfile, line);
+			contents.append(line);
+	}
+
+	myInputfile.close();
+	return SUCCESS;
+
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream myOutputfile;
+	myOutputfile.open(filename.c_str());
+
+	if (!myOutputfile.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	for (int i = 0; i < myEntryVector.size(); i++){
+		myOutputfile << myEntryVector.at(i);
+		myOutputfile << "\n";
+		;
+	}
+
+	myOutputfile.close();
+	return SUCCESS;
 }
 
 
